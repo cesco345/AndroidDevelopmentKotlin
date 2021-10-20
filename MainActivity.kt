e9ab0b0c7aa8b7d3bbcf0d0db8544252afe19299
Francesco Piscani <fpiscani@gmail.com>
2021-10-20 12:13:42 -0400

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,45 @@
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.diceroller.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage:ImageView
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        diceImage = binding.diceImage
+        val rollBtn:Button = binding.rollButton
+
+        rollBtn.setOnClickListener { rollDice() }
+
+    }
+
+    private fun rollDice() {
+        val drawableImage = when(Random.nextInt(1,7)){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+
+            else -> R.drawable.empty_dice
+        }
+
+        diceImage.setImageResource(drawableImage)
+
+    }
+
+
+}
